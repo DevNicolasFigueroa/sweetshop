e44d8e826bc6e8f6f5b46f8df11520dc0f4f48ed
BlackCorsair22 <dev.figueroanicolas22@gmail.com>
2026-06-10 00:19:27 -0400

    feat: setup frontend with vite react typescript and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-center py-10 text-pink-500">
+        🎂 SweetShop
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // 1. Agregamos el plugin de Tailwind aquí
+  ],
+});
